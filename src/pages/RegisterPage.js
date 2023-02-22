cac61a02e1cd4109cacb0f8c5559f429f787013b
Developer-Zahid <coderexpertzahid@gmail.com>
2023-02-22 11:55:41 +0600

    register with firebase done

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import '../assets/css/pages/auth-page.css'
@@ -13,6 +13,14 @@ const RegisterPage = () => {
   })
 
   const [loading, setLoading] = useState(false)
+
+  const emptyInputValue = ()=>{
+    setInputValue({
+      register_email: '',
+      register_name: '',
+      register_password: ''
+    })
+  }
 
   const handelInputValue = (e)=>{
     const {name, value} = e.target
@@ -48,35 +56,43 @@ const RegisterPage = () => {
   }
 
   const auth = getAuth();
+  const navigate = useNavigate();
 
   const handelSubmitClick = () =>{
-    if(inputValue.register_email === ''){
+    if(!inputValue.register_email){
       showError("Email is requred")
     }else if(!isValidEmail(inputValue.register_email)){
       showError("Please enter a valid email")
     }
-    if(inputValue.register_name === ''){
+
+    if(!inputValue.register_name){
       showError("Full name is requred")
     }
-    if(inputValue.register_password === ''){
+
+    if(!inputValue.register_password){
       showError("Password is requred")
     }else if(!isValidPassword(inputValue.register_password)){
-      showError("Please enter a valid password")
+      showError("Password must be at least 8 characters and contains 1 capital letter, 1 small letter")
     }
 
-    if((inputValue.register_email !== '' && isValidEmail(inputValue.register_email)) && inputValue.register_name !== '' && (inputValue.register_password !== '' && isValidPassword(inputValue.register_password))){
+    if((inputValue.register_email && isValidEmail(inputValue.register_email)) && inputValue.register_name && (inputValue.register_password && isValidPassword(inputValue.register_password))){
+      setLoading(true);
       createUserWithEmailAndPassword(auth, inputValue.register_email, inputValue.register_password)
       .then((userCredential)=>{
         const user = userCredential.user;
         showSuccess("Registered successfully");
+        emptyInputValue();
+        setLoading(false);
+
         sendEmailVerification(auth.currentUser)
         .then(() => {
-          // Email verification sent!
-          // ...
+          showSuccess("Email verification send!");
+          navigate('/');
         });
       })
       .catch((error) => {
         const errorCode = error.code;
+        setLoading(false);
         if(errorCode.includes('auth/email-already-in-use')){
           showError("Email is already exist")
         }
@@ -95,9 +111,9 @@ const RegisterPage = () => {
                 </div>
                 <div className="col-lg-5 col-md-7 col-sm-10 text-center mx-auto mb-auto">
                     <form action="#!" className='auth__form'>
-                      <AuthInput label="Email address" type="email" name="register_email" pattern="^([\w\.])+@[\w]+\.[\w\.\-]{2,4}$" onChange={handelInputValue} required />
-                      <AuthInput label="Full name" type="text" name="register_name" onChange={handelInputValue} required />
-                      <AuthInput label="Password" type="password" name="register_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" onChange={handelInputValue} required />
+                      <AuthInput label="Email address" type="email" name="register_email" value={inputValue.register_email} pattern="^([\w\.])+@[\w]+\.[\w\.\-]{2,4}$" onChange={handelInputValue} required />
+                      <AuthInput label="Full name" type="text" name="register_name" value={inputValue.register_name} onChange={handelInputValue} required />
+                      <AuthInput label="Password" type="password" name="register_password" value={inputValue.register_password} autocomplete="new-password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" onChange={handelInputValue} required />
                         {
                           loading ?
                           <button type='button' className='btn auth__btn btn-block w-100 rounded-pill' disabled>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import HomePage from "../pages/HomePage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
             <Route path="/" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/home" element={<HomePage />} />
         </Route>
     )
 );
